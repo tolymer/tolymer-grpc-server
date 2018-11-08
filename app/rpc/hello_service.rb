b8c5ee818e3ad4a8a06be0cae50e4812e7358d3b
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-11-09 00:13:12 +0900

    Add rpc spec

--- app/rpc/hello_service.rb
+++ app/rpc/hello_service.rb
@@ -1,9 +1,7 @@
-module Tolymer
-  class HelloService < Gruf::Controllers::Base
-    bind Tolymer::Hello::Service
+class HelloService < Gruf::Controllers::Base
+  bind Tolymer::Hello::Service
 
-    def greet
-      Tolymer::GreetResponse.new(id: 1, name: 'hello')
-    end
+  def greet
+    Tolymer::GreetResponse.new(id: request.message.id, name: 'hello')
   end
 end
