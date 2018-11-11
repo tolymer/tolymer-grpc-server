c239a00f73f8b60cfed8486d26976368cc2657d8
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-11-11 21:35:02 +0900

    Rename member to participant

--- db/schema/scores.rb
+++ db/schema/scores.rb
@@ -1,9 +1,9 @@
 create_table 'scores', force: :cascade do |t|
   t.bigint  'game_id',   null: false
-  t.bigint  'member_id', null: false
+  t.bigint  'participant_id', null: false
   t.integer 'point',     null: false
 
   t.timestamps
 end
 
-add_index 'scores', ['game_id', 'member_id'], unique: true, using: :btree
+add_index 'scores', ['game_id', 'participant_id'], unique: true, using: :btree
--- db/schema/tips.rb
+++ db/schema/tips.rb
@@ -1,9 +1,9 @@
 create_table 'tips', force: :cascade do |t|
-  t.bigint  'event_id',  null: false
-  t.bigint  'member_id', null: false
-  t.integer 'point',     null: false
+  t.bigint  'event_id',       null: false
+  t.bigint  'participant_id', null: false
+  t.integer 'point',          null: false
 
   t.timestamps
 end
 
-add_index 'tips', ['event_id', 'member_id'], unique: true, using: :btree
+add_index 'tips', ['event_id', 'participant_id'], unique: true, using: :btree
